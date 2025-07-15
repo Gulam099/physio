c62e6d5786311376fb3670663b26e8a166b3246d
unknown <razag5537@gmail.com>
2025-07-15 13:10:44 +0530

    this is new commit

--- src/app/(dashboard)/login/page.tsx
+++ src/app/(dashboard)/login/page.tsx
@@ -266,7 +266,7 @@ export default function LoginComponent() {
             </Button>
             <Box sx={{ textAlign: "center" }}>
               <Typography variant="body2" color="text.secondary">
-                Don't have an account?{" "}
+                Don&apos;t have an account?{" "}
                 <Link
                   href="#"
                   variant="body2"
